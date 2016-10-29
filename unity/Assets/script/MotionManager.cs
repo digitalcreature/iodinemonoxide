411b6ddc73bca4940c19b759a19c1e5dd3aa0b02
Tanner Grehawick <tgrehawi@gmail.com>
2016-10-29 17:54:10 -0400

    MotionManager

--- unity/Assets/script/MotionManager.cs
+++ unity/Assets/script/MotionManager.cs
@@ -3,4 +3,31 @@ using Leap;
 
 public class MotionManager : MonoBehaviour {
 
+	private Leap.Controller controller;
+
+	void Awake() {
+		controller = new Leap.Controller();
+	}
+
+	private Vector3 vec(Leap.Vector v) {
+		return new Vector3(
+			v.x,
+			v.y,
+			-v.z
+		) / 10;
+	}
+
+	void Update() {
+		if (controller != null) {
+			Frame frame = controller.Frame();
+			HandList hands = frame.Hands;
+			for (int h = 0; h < hands.Count; h ++) {
+				Hand hand = hands[h];
+				Vector3 pos = vec(hand.PalmPosition);
+				Vector3 normal = vec(hand.PalmNormal);
+			}
+		}
+	}
+
+
 }
